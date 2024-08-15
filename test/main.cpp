355e40be34a9d66cd04c47d610b8a7a5b489dfba
Nico <ndbykh@gmail.com>
2024-08-15 16:04:51 +0200

    added a test

--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,18 @@
 #include <nlohmann/json.hpp>
 #include <fstream>
 #include <vector>
+#include <iostream>
+#include <cstdlib>
 
 int main() {
     auto json = nlohmann::json{};
     const auto value = 42;
     json["node1"]["value"] = value;
     const auto values = std::vector<int>{1,3,5,6,9};
-    json["node1"]["values"] = values;
+    json["node1"]["values"] = {1,3,5,6,9};
     std::ofstream("test.json") << json.dump();
-    auto json1 = nlohmann::json{"test.json"};
+    const auto json1 = nlohmann::json::parse(std::ifstream("test.json"));
+    std::system("rm test.json");
     if (json1["node1"]["value"] != 42) {
         return -1;
     }
@@ -18,7 +21,7 @@ int main() {
     }
     auto iValue = 0;
     for (const auto valueFromList : values) {
-        if (json1["node1"]["list"].at(iValue++) != valueFromList) {
+        if (json1["node1"]["values"].at(iValue++) != valueFromList) {
             return -1;
         }
     }
