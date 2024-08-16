02d1c3de6d07760c5201360715c54160cf02092f
Nico <ndbykh@gmail.com>
2024-08-16 08:23:13 +0200

    added format check for Python files

--- scripts/check-format.sh
+++ scripts/check-format.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+exit_code=0
 find ./test -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format --dry-run --Werror -i
 if [[ $? -ne 0 ]]; then
+    exit_code=1
+fi
+
+find ./scripts -iname '*.py' | xargs pycodestyle --first
+if [[ $? -ne 0 ]]; then
+    exit_code=1
+fi
+
+if [[ $exit_code -ne 0 ]]; then
     echo "[FAILED] Please fix format: run "
     echo "  bash ./scripts/format-files.sh"
-    exit 1
+    exit $exit_code
 fi
--- scripts/format-files.sh
+++ scripts/format-files.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 find ./test -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -i
+find ./scripts -iname '*.py' | xargs autopep8 -i
