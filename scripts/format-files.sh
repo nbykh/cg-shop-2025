70a89f7f9f80c6b1f33d604546873ff7fbb1a27c
Nico <ndbykh@gmail.com>
2024-08-31 20:31:35 +0200

    fixed formatting script

--- scripts/format-files.sh
+++ scripts/format-files.sh
@@ -2,5 +2,6 @@
 
 # format C++ files
 find ./test -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -i
+find ./src -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -i
 # format Python files
 black scripts/*.py
